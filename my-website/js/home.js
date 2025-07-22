d1b276187bc1f24c5e9e3ab5c14b1096742833a9
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-22 12:11:08 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '69417ec87bd388d1e39d8d8307479574';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
